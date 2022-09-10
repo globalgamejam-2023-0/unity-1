ad0c0eec29a05efcc689dc01d25c95ac670ee710
Torkil <torkil.tho20@student.setur.fo>
2022-09-10 15:21:48 +0100

    menu and dialogue + dialoguemanager

---
+++ Assets/Scripts/Dialog/DialogueManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+
+    private Queue<string> clues; // first in first out
+
+    private void Start()
+    {
+        clues = new Queue<string>();
+        
+        
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        //Debug.Log("Starting dialogue");
+
+        nameText.text = dialogue.clue;
+
+        clues.Clear();
+
+        foreach (string clue in dialogue.cluesForQueue)
+        {
+            clues.Enqueue(clue);
+
+        }
+
+        DisplayNextClue(); 
+
+    }
+
+    public void DisplayNextClue()
+    {
+        if (clues.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string clue = clues.Dequeue();
+        dialogueText.text = clue;
+
+        Debug.Log(clue);
+
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("remove Dialogue");
+    }
+
+}
