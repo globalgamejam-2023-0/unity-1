b537f7d1e8bfe0d666e8ee2783816c0db8356a74
Rúni Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-09-11 14:52:58 +0100

    Font added. Prototype more or less complete

---
+++ Assets/Scripts/OfficeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OfficeScene : MonoBehaviour
+{
+    public void gotit()
+    {
+        Debug.Log("Back has been pressed");
+        SceneManager.LoadScene("prototype5-rkh");
+    }
+    
+}
